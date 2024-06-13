99700ac4bf0d4cd65fb46f1c6340a4d364c7dee9
NEM-ARS <nemkovich2005@mail.ru>
2024-06-13 13:59:08 +0300

    form processing

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-let btn = document.querySelector('.secton__button');
-let modal = document.querySelector('.modal');
-let close = document.querySelector('.form__close__btn');
-btn.onclick = ()=> modal.classList.add('show')
-close.onclick = ()=> modal.classList.remove('show')
+const btn = document.querySelector('.secton__button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.form__close__btn');
+const form = document.getElementById('form');
+
+btn.onclick = () => modal.classList.add('show');
+
+close.onclick = () => formReset();
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    const values = new FormData(form);
+
+    for (let value of values) {
+        console.log(value);
+    }
+
+    formReset()
+};
+
+function formReset() {
+    form.reset();
+    modal.classList.remove('show');
+}
